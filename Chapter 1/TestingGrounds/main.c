cdaf9e4874fed896e82e172b67da611783ba6536
Radin Pourfarzi <rpourfarzi2@gmail.com>
2026-02-16 16:40:24 -0800

    testing

--- Chapter 1/TestingGrounds/main.c
+++ Chapter 1/TestingGrounds/main.c
@@ -1,71 +1,19 @@
 #include <stdio.h>
 
-#define IN      1
-#define OUT     0
-
-power(base, n)
-int base, n;
-{
-    return base * n;
-}
+#define MAX     10000
 
 int main()
 {
-    signed char c;
-    int nl, nw, nc, wl, state;
-    int wordLength[9];
-    int charFreq[26];
-
-    state = OUT;
-    nl = nw = nc = wl = 0;
-
-    for(int i = 0; i < 9; i++) {
-        wordLength[i] = 0;
-        charFreq[i] = 0;
-    }
-    for(int i = 9; i < 26; i++) {
-        charFreq[i] = 0;
-    }
+    int nc, nl, c;
+    nc = nl = 0;
 
     while((c = getchar()) != EOF) {
-        ++nc;
-        if(c == '\n') {
+        if (c == '\n')
             ++nl;
-        }
-        if(c == ' ' || c == '\n' || c == '\t') {
-            state = OUT;
-            if(wl >= 1) {
-                ++wordLength[wl];
-            }
-            wl = 0;
-        }
-        else if(state == OUT) {
-            state = IN;
-            ++nw;
-            ++charFreq[c - 'a'];
-        }
-        if(state == IN) {
-            ++wl;
-        }
+        ++nc;
     }
-
-    printf("nl: %d, nw: %d, nc: %d\n", nl, nw, nc);
-
-    for(int i = 0; i < 10; i++) {
-        printf("%d|", i);
-        for(int j = 0; j < wordLength[i]; j++) {
-            printf("[\t]");
-        }
-        printf("\n");
-    }
-    printf("\n\n");
-    for(int i = 0; i < 26; i++) {
-        printf("%c|", (i + 'a'));
-        for(int j = 0; j < charFreq[i]; j++) {
-            printf("[\t]");
-        }
-        printf("\n");
-    }
+    
+    printf("Character count %d\nLine count %d\n", nc, nl);
 
     return 0;
 }
