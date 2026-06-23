fbf8fd7afc27dbe1f7df293c73da69861ce979ed
Radin Pourfarzi <97715025+RadinPourfarzi@users.noreply.github.com>
2026-06-23 16:26:57 -0700

    Parser Frame

--- Projects/Project 2/parser.c
+++ Projects/Project 2/parser.c
@@ -1,1 +1,44 @@
 #include "parser.h"
+
+AST_Node *assign(Parser *p)
+{
+    AST_Node *left;
+
+    left = expression(p);
+
+    return left;
+}
+
+AST_Node *expression(Parser *p)
+{
+    AST_Node *left;
+
+    left = term(p);
+
+    return left;
+}
+
+AST_Node *term(Parser *p)
+{
+    AST_Node *left;
+
+    left = exponent(p);
+
+    return left;
+}
+
+AST_Node *exponent(Parser *p)
+{
+    AST_Node *left;
+
+    left = primary(p);
+
+    return left;
+}
+
+AST_Node *primary(Parser *p)
+{
+    AST_Node *node;
+
+    return node;
+}
